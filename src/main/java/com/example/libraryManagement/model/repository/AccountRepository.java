5a9d821f1d948adc9ecdedf965b78825e63b7de4
anhngo0 <anhngo9203@gmail.com>
2024-07-25 13:41:53 +0700

    fix reduce no-use paths

--- src/main/java/com/example/libraryManagement/model/repository/AccountRepository.java
+++ src/main/java/com/example/libraryManagement/model/repository/AccountRepository.java
@@ -2,8 +2,10 @@ package com.example.libraryManagement.model.repository;
 
 import com.example.libraryManagement.model.entity.Account;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Optional;
 
@@ -14,5 +16,5 @@ public interface AccountRepository extends ParentRepository<Account,Long> {
     Optional<Account> findOneByResetKey(String resetKey);
 
     @Query("SELECT a FROM Account a JOIN a.profile p WHERE LOWER(p.email) = LOWER(:email)")
-    Optional<Account> findOneByProfileEmailIgnoreCase(String email);
+    Optional<Account> findOneByProfileEmailIgnoreCase( @Param("email") String email);
 }
